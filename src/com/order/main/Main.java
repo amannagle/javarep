424116f3984446b73dd95439c3d0dde9d71bc893
aman <aman.nagle1999@gmail.com>
2021-09-30 17:00:41 +0530

    main commit

---
+++ src/com/order/main/Main.java
@@ -0,0 +1,13 @@
+package com.order.main;
+
+import com.order.entities.Orders;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Orders o1=new Orders();
+		o1.setOid(0);
+		System.out.println(o1.getOid());
+	}
+
+}
